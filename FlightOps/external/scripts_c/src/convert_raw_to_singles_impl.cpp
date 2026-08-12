f7b6939132e05f744e9633fc9c6deb7b23dee1ca
hyebin-jeon <hyebin@uchicago.edu>
2026-08-12 10:15:50 -0400

    stg1 TTree renamed

--- FlightOps/external/scripts_c/src/convert_raw_to_singles_impl.cpp
+++ FlightOps/external/scripts_c/src/convert_raw_to_singles_impl.cpp
@@ -87,7 +87,8 @@ public:
 			hFile = new TFile(fName.c_str(), "RECREATE");
 			int bs = 512*1024;
 
-			hData = new TTree("data", "Event List", 2);
+			//hData = new TTree("data", "Event List", 2);
+			hData = new TTree("stg1", "Event List", 2);
 			hData->Branch("step1", &brStep1, bs);
 			hData->Branch("step2", &brStep2, bs);
 			hData->Branch("time", &brTime, bs);
